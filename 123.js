067f1beb68d1e46a12748e3502765ccf9c82b658
银河灿灿 <65111206+lifeafter619@users.noreply.github.com>
2021-12-17 21:13:23 +0800

    Create 123.js

---
+++ 123.js
@@ -0,0 +1,14 @@
+<script src="http://www.guofei.site/star_counter/star_counter.js"></script> <!--引用js代码-->
+<script>
+    function func_1() {
+        document.getElementById("star_counter").innerHTML = 'If not print for seconds, please refresh';
+        github_id = document.getElementById("user").value;
+        document.getElementById("star_counter").innerHTML = cal_github_star(github_id);
+    }
+</script>
+
+
+Input github id: <input name="user" value="guofei9987" type="text" id="user">
+<input name="Button" type="button" value="Calculate" onClick="func_1()">
+
+<p id="star_counter"></p>
